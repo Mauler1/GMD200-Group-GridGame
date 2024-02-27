c7f1b18283aa1c4fe49dcae1abca4dced95261a3
Nicholas Mastrangelo <nicholas.mastrangelo@mymail.champlain.edu>
2024-02-27 17:08:26 -0500

    Added my dev scene and 2 scripts

---
+++ GMD200-Group-GridGame-ChessFight/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiPathing : MonoBehaviour
+{
+    // What this will do is this will be going through each path once the path is created and move to every tile
+    // this is in order from the starting place to the ending place
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
