78629099c5c747dec06f27ebff159fd1c2e1d9f0
Nicholas Mastrangelo <nicholas.mastrangelo@mymail.champlain.edu>
2024-03-01 17:15:55 -0500

    added a grid to my scene

--- GMD200-Group-GridGame-ChessFight/Assets/Scripts/EnemyPathing.cs
+++ GMD200-Group-GridGame-ChessFight/Assets/Scripts/EnemyPathing.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class AiPathing : MonoBehaviour
 {
+    private int pathLength;
+    private int[] enemyCoords;
     // What this will do is this will be going through each path once the path is created and move to every tile
     // this is in order from the starting place to the ending place
+
+    // first the grid will need to be set up for this 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // pathLength needs to get the coordinates for the pathing
+        // this will set up the dynamic array
     }
 
     // Update is called once per frame
