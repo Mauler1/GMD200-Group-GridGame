524ac95f8939f58c2d6685caddec4bedecd9ce07
Abrahm Routhier <ajrouthier2505@outlook.com>
2024-03-05 16:54:36 -0500

    Fixed pawn attack spawning and cleaned up piece
    Removed unnecessary  piece fluff and fixed the spawning for Pawn's attack squares

--- GMD200-Group-GridGame-ChessFight/Assets/Scripts/Piece.cs
+++ GMD200-Group-GridGame-ChessFight/Assets/Scripts/Piece.cs
@@ -4,21 +4,6 @@ using UnityEngine;
 
 public abstract class Piece : MonoBehaviour
 {
-    private int damage;
-    private int cost;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public abstract void spawn();
 
     public abstract int getDamage();
