439a74c4425951b35cc82ba51553b2fdd8b397d8
Nicholas Mastrangelo <nicholas.mastrangelo@mymail.champlain.edu>
2024-03-05 16:56:40 -0500

    Added new enemySpawner Script

---
+++ GMD200-Group-GridGame-ChessFight/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+
+}
